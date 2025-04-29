854fb444bbac32893ba0b412cf493ef5a3c7cc88
karol-white <0001129651459sp@al.educacao.sp.gov.br>
2025-04-29 12:12:36 -0300

    fizemos variaveis, nome e idade

---
+++ function.py
@@ -0,0 +1,61 @@
+def soma():
+    nun_01 = 10
+    nun_02 = 21
+
+    resultado = nun_01 + nun_02
+
+    return resultado
+
+def somarDoisNumeros(nota_01, nota_02):
+
+
+    resultado = nota_01 + nota_02
+
+    return print(resultado)
+
+# somarDoisNumeros(23, 97)
+
+def multiplicar():
+    nun_01 = 10
+    nun_02 = 21
+
+    resultado = nun_01 * nun_02
+
+    return resultado
+
+def multiplicarDoisNumeros(nota_01, nota_02):
+
+
+    resultado = nota_01 * nota_02
+
+    return print(resultado)
+
+# multiplicarDoisNumeros(10, 2)
+
+def divisão():
+    nun_01 = 10
+    nun_02 = 21
+
+    resultado = nun_01 / nun_02
+
+    return resultado
+
+def dividirDoisNumeros(nota_01, nota_02):
+
+
+    resultado = nota_01 / nota_02
+
+    return print(resultado)
+
+# dividirDoisNumeros(10, 2)
+
+
+def cadastrar(nome, idade):
+    data = {
+        "nome": nome,
+        "idade": idade,
+    }
+    return print(data)
+ 
+cadastrar("ana", 16)
+
